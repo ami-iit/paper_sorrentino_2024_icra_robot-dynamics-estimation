ed547179882aac39077e7e7ab36c99e42340e031
Ines <ines.sorrentino10@gmail.com>
2023-09-13 09:36:52 +0200

    Add python application to use RDE through bindings

---
+++ python/rde_synthetic_application.py
@@ -0,0 +1,333 @@
+# Author Ines Sorrentino - 2023
+
+import h5py
+import numpy as np
+import manifpy
+from progressbar import ProgressBar
+import pickle
+import time
+
+import bipedal_locomotion_framework.bindings as blf
+import idyntree.bindings as idyn
+import icub_models
+
+import matplotlib.pyplot as plt
+
+def populate_numerical_data(file_object):
+    data = {}
+
+    for key, value in file_object.items():
+        if not (type(file_object[key]) is h5py._hl.group.Group):
+            data[key] = np.squeeze(np.array(value)).T
+        else:
+            if (key==".." or key=="test"):
+                data = populate_numerical_data(file_object=value)
+            else:
+                data[key] = populate_numerical_data(file_object=value)
+
+    return data
+
+def build_kin_dyn(robot_name, param_handler, robot_model_path):
+    if (robot_model_path == ""):
+        robot_model_path = icub_models.get_model_file(robot_name)
+
+    joint_list = param_handler.get_parameter_vector_string("joint_list")
+    ft_list = param_handler.get_group("FT").get_parameter_vector_string("associated_joints")
+
+    ml = idyn.ModelLoader()
+    ml.loadReducedModelFromFile(str(robot_model_path), joint_list + ft_list)
+
+    # Create kindyn for full model
+    kindyn = idyn.KinDynComputations()
+    kindyn.loadRobotModel(ml.model())
+    ok = kindyn.setFrameVelocityRepresentation(idyn.BODY_FIXED_REPRESENTATION)
+    if (not ok):
+        raise ValueError("BODY_FIXED_REPRESENTATION not set")
+
+    # create submodel list
+    sub_model_creator = blf.robot_dynamics_estimator.SubModelCreator()
+    sub_model_creator.setModelAndSensors(ml.model(), ml.sensors())
+    sub_model_creator.setKinDyn(kindyn)
+    sub_model_creator.createSubModels(param_handler)
+
+    sub_model_list = sub_model_creator.getSubModelList()
+
+    kindyn_wrapper_list = []
+    for idx in range(len(sub_model_list)):
+        kindyn_sm = blf.robot_dynamics_estimator.KinDynWrapper()
+        if (not kindyn_sm.setModel(sub_model_list[idx])):
+            raise ValueError("kindyn_sm.initialize")
+        kindyn_wrapper_list.append(kindyn_sm)
+
+    idynEstimator = idyn.ExtWrenchesAndJointTorquesEstimator()
+    idynEstimator.setModelAndSensors(ml.model(), ml.sensors())
+
+    return kindyn, sub_model_list, kindyn_wrapper_list, idynEstimator
+
+def build_initial_state(data, handler, idynEstimator):
+    x0 = blf.robot_dynamics_estimator.RobotDynamicsEstimatorOutput()
+
+    joint_list_config = handler.get_parameter_vector_string("joint_list")
+    n_joints = len(joint_list_config)
+    joint_list_dataset = joint_list_config
+
+    joints_idx_in_dataset = []
+
+    for i in range(n_joints):
+        idx = [n for n, x in enumerate(joint_list_dataset) if joint_list_config[i] in x]
+        joints_idx_in_dataset.append(idx)
+
+    x0.ds = data['sim']['ds'][0, joints_idx_in_dataset]
+
+    s = data['sim']['s'][0, joints_idx_in_dataset]
+    dds = data['sim']['dds'][0, joints_idx_in_dataset]
+
+    x0.tau_m = data['sim']['tau_m'][0, joints_idx_in_dataset]
+
+    x0.tau_F = data['sim']['tau_F'][0, joints_idx_in_dataset]
+
+    # Set contact information and specify the unknown wrench on the base link
+    base_link = handler.get_parameter_string("base_link")
+    fixedFrameIdx = idynEstimator.model().getFrameIndex(base_link)
+    fullBodyUnknowns = idyn.LinkUnknownWrenchContacts(idynEstimator.model())
+    fullBodyUnknowns.clear()
+    fullBodyUnknowns.addNewUnknownFullWrenchInFrameOrigin(idynEstimator.model(), fixedFrameIdx)
+
+    # Initialize variables for estimation
+    expectedFT = idyn.SensorsMeasurements(idynEstimator.sensors())
+    estimatedContacts = idyn.LinkContactWrenches(idynEstimator.model())
+    estimatedTau = idyn.JointDOFsDoubleArray(idynEstimator.model())
+
+    # TO BE CHANGED IN FLOATING BASE CASA
+    gravity = idyn.Vector3()
+    gravity.zero()
+    gravity[2] = -blf.math.StandardAccelerationOfGravitation
+
+    s_idyn = idyn.JointPosDoubleArray(n_joints)
+    ds_idyn = idyn.JointDOFsDoubleArray(n_joints)
+    dds_idyn = idyn.JointDOFsDoubleArray(n_joints)
+
+    for j in range(n_joints):
+        s_idyn.setVal(j, s[j].item())
+        ds_idyn.setVal(j, x0.ds[j].item())
+        dds_idyn.setVal(j, dds[j].item())
+
+    idynEstimator.updateKinematicsFromFixedBase(s_idyn, ds_idyn, dds_idyn, fixedFrameIdx, gravity)
+    idynEstimator.computeExpectedFTSensorsMeasurements(fullBodyUnknowns, expectedFT, estimatedContacts, estimatedTau)
+
+    ft_associated_joints = handler.get_group("FT").get_parameter_vector_string("associated_joints")
+    ft_list = handler.get_group("FT").get_parameter_vector_string("names")
+
+    ftFromModel = dict()
+    ft_offsets = dict()
+    temp1 = dict()
+    temp2 = dict()
+    ftWrench = idyn.Wrench()
+    for ft in ft_list:
+        ftIndex = idynEstimator.sensors().getSensorIndex(idyn.SIX_AXIS_FORCE_TORQUE, ft)
+        assert expectedFT.getMeasurement(idyn.SIX_AXIS_FORCE_TORQUE, ftIndex, ftWrench)
+        ftFromModel[ft] = ftWrench.toNumPy()
+
+        ft_temp = data['sim']['fts'][ft][0]
+
+        ft_offsets[ft] = data['sim']['fts'][ft][0] - ftFromModel[ft]
+        ft_offsets[ft] = ft_temp - ftFromModel[ft]
+        temp1[ft] = ftFromModel[ft]
+        temp2[ft + '_bias'] = ft_offsets[ft]
+
+    x0.ftWrenches = temp1
+    # x0.ftWrenchesBiases = temp2
+
+    acc_list = handler.get_group("ACCELEROMETER").get_parameter_vector_string("names")
+    temp3 = dict()
+    for acc in acc_list:
+        temp3[acc+'_bias'] = np.zeros(3)
+    x0.accelerometerBiases = temp3
+
+    gyro_list = handler.get_group("GYROSCOPE").get_parameter_vector_string("names")
+    temp4 = dict()
+    for gyro in gyro_list:
+        temp4[gyro + '_bias'] = np.zeros(3)
+    x0.gyroscopeBiases = temp4
+
+    contact_list = handler.get_group("EXTERNAL_CONTACT").get_parameter_vector_string("frames")
+    temp5 = dict()
+    for contact in contact_list:
+        temp5[contact] = np.zeros(6)
+    x0.contactWrenches = temp5
+
+    return x0, ft_offsets, joints_idx_in_dataset
+
+def build_estimator_input(data, index, handler, ft_offsets, joints_idx_in_dataset):
+    input_rde = blf.robot_dynamics_estimator.RobotDynamicsEstimatorInput()
+
+    input_rde.basePose = manifpy.SE3(position=[0.0, 0.0, 0.0], quaternion=[0.0, 0, 0, 1.0])
+    input_rde.baseVelocity = manifpy.SE3Tangent([0.0]*6)
+    input_rde.baseAcceleration = manifpy.SE3Tangent([0.0]*6)
+
+    input_rde.jointPositions = data['sim']['s'][index, joints_idx_in_dataset]
+    input_rde.jointVelocities = data['sim']['ds'][index, joints_idx_in_dataset]
+
+    input_rde.motorCurrents = data['sim']['i_m'][index, joints_idx_in_dataset]
+
+    ft_list = handler.get_group("FT").get_parameter_vector_string("names")
+    temp1 = dict()
+    for ft in ft_list:
+        ft_temp = data['sim']['expected_fts'][ft][index]
+        temp1[ft] = ft_temp - ft_offsets[ft]
+    input_rde.ftWrenches = temp1
+
+    acc_frames = handler.get_group("ACCELEROMETER").get_parameter_vector_string("frames")
+    acc_names = handler.get_group("ACCELEROMETER").get_parameter_vector_string("names")
+    temp2 = dict()
+    i = 0
+    for acc in acc_frames:
+        temp2[acc_names[i]] = data['sim']['accelerometers'][acc+'_acc'][index]
+        i = i + 1
+    input_rde.linearAccelerations = temp2
+
+    gyro_frames = handler.get_group("GYROSCOPE").get_parameter_vector_string("frames")
+    gyro_names = handler.get_group("GYROSCOPE").get_parameter_vector_string("names")
+    temp3 = dict()
+    i = 0
+    for gyro in gyro_frames:
+        temp3[gyro_names[i]] = data['sim']['gyros'][gyro+'_gyro'][index]
+        i = i + 1
+    input_rde.angularVelocities = temp3
+
+    return input_rde
+
+def parse_output(output, handler):
+    rde = dict()
+
+    rde['ds'] = []
+    rde['tau_m'] = []
+    rde['tau_F'] = []
+    rde['tau_j'] = []
+    rde['ft'] = dict()
+    rde['ft_bias'] = dict()
+    rde['acc_bias'] = dict()
+    rde['gyro_bias'] = dict()
+    rde['contact'] = dict()
+
+    ft_list = handler.get_group("FT").get_parameter_vector_string("names")
+    acc_list = handler.get_group("ACCELEROMETER").get_parameter_vector_string("names")
+    gyro_list = handler.get_group("GYROSCOPE").get_parameter_vector_string("names")
+    contact_list = handler.get_group("EXTERNAL_CONTACT").get_parameter_vector_string("frames")
+
+    for ft in ft_list:
+        rde['ft'][ft] = []
+    #     rde['ft_bias'][ft] = []
+
+    for acc in acc_list:
+        rde['acc_bias'][acc] = []
+
+    for gyro in gyro_list:
+        rde['gyro_bias'][gyro] = []
+
+    for contact in contact_list:
+        rde['contact'][contact] = []
+
+    for index in range(len(output)):
+        rde['ds'].append(output[index].ds)
+        rde['tau_m'].append(output[index].tau_m)
+
+        n_joints = len(rde['ds'][0])
+
+        rde['tau_F'].append(output[index].tau_F)
+
+        # print(output[index].tau_F)
+
+        rde['tau_j'].append(output[index].tau_m - output[index].tau_F)
+
+        for ft in ft_list:
+            rde['ft'][ft].append(output[index].ftWrenches[ft])
+            # rde['ft_bias'][ft].append(output[index].ftWrenchesBiases[ft+'_bias'])
+
+        for acc in acc_list:
+            rde['acc_bias'][acc].append(output[index].accelerometerBiases[acc+'_bias'])
+
+        for gyro in gyro_list:
+            rde['gyro_bias'][gyro].append(output[index].gyroscopeBiases[gyro+'_bias'])
+
+        for contact in contact_list:
+            rde['contact'][contact].append(output[index].contactWrenches[contact])
+
+    rde['ds'] = np.array(rde['ds'])
+    rde['tau_m'] = np.array(rde['tau_m'])
+    rde['tau_F'] = np.array(rde['tau_F'])
+    rde['tau_j'] = np.array(rde['tau_j'])
+    for ft in ft_list:
+        rde['ft'][ft] = np.array(rde['ft'][ft])
+    #     rde['ft_bias'][ft] = np.array(rde['ft_bias'][ft])
+    for acc in acc_list:
+        rde['acc_bias'][acc] = np.array(rde['acc_bias'][acc])
+    for gyro in gyro_list:
+        rde['gyro_bias'][gyro] = np.array(rde['gyro_bias'][gyro])
+    for contact in contact_list:
+        rde['contact'][contact] = np.array(rde['contact'][contact])
+
+    return rde
+
+def main():
+    robot = 'four_joints_two_sensors'
+    
+    param_handler = blf.parameters_handler.YarpParametersHandler()
+    assert param_handler.set_from_filename("config/"+robot+"/config.ini")
+
+    robot_name = param_handler.get_group("GENERAL").get_parameter_string("robot_name")
+    try:
+        robot_model_path = param_handler.get_group("GENERAL").get_parameter_string("urdf_path")
+    except:
+        robot_model_path = ""
+
+    # Create sub-models related objects
+    [kindyn, sub_model_list, kindyn_wrapper_list, idynEstimator] = build_kin_dyn(robot_name, param_handler.get_group("MODEL"), robot_model_path)
+
+    # Crete estimator
+    estimator = blf.robot_dynamics_estimator.RobotDynamicsEstimator.build(param_handler,
+                                                                          kindyn,
+                                                                          sub_model_list,
+                                                                          kindyn_wrapper_list)
+
+    # Load dataset
+    dataset = param_handler.get_group("GENERAL").get_parameter_string("dataset")
+    f = h5py.File(dataset, 'r')
+    data = populate_numerical_data(f)
+
+    # Build and set initial state
+    [initial_state, ft_offset, joints_idx_in_dataset] = build_initial_state(data, param_handler.get_group("MODEL"), idynEstimator)
+    ok = estimator.setInitialState(initial_state)
+    if not ok:
+        raise ValueError("Failed setting initial state")
+
+    output_rde = []
+
+    # last_index = data['joints_state']['positions']['data'].shape[0]
+    # if last_index > 20000:
+    #     last_index = 20000
+
+    pbar = ProgressBar()
+    for index in pbar(range(data['sim']['s'].shape[0])):
+    # for index in pbar(range(10)):
+        index1 = index
+
+        input_rde = build_estimator_input(data, index1, param_handler.get_group("MODEL"), ft_offset, joints_idx_in_dataset)
+        ok = estimator.set_input(input_rde)
+        if not ok:
+            raise ValueError("Failed setting the input")
+
+        ok = estimator.advance()
+        if not ok:
+            raise ValueError("Failed advancing")
+
+        output_rde.append(estimator.get_output())
+
+    rde = parse_output(output_rde, param_handler.get_group("MODEL"))
+
+    timestr = time.strftime("%Y%m%d_%H%M%S")
+    with open('output_estimation/' + robot_name + '/workspace_' + timestr + '.pickle', 'wb') as handle:
+        pickle.dump([rde, data], handle)
+
+if __name__ == "__main__":
+    main()
